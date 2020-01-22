40a2e3dfc216e9638f80bb54f7b508b1a0402661
swishman <matthew23fishman@Gmail.com>
2020-01-22 15:44:39 -0500

    A1: Random Colors on Mouseover

--- assignments/pixel-painter-pro/js/script.js
+++ assignments/pixel-painter-pro/js/script.js
@@ -3,11 +3,12 @@
 window.onload = setup;
 
 function setup() {
-  console.log("Taking attendance... Present!")
 
-  for (let i = 0; i < 1001; i++){
+  console.log("Taking attendance... Present!");
+
+  for (let i = 0; i < 1001; i++) {
     let pixel = document.createElement('div');
-    pixel.setAttribute ('class', 'pixel');
+    pixel.setAttribute('class', 'pixel');
     pixel.addEventListener('mouseover', paint);
 
     document.body.appendChild(pixel);
@@ -16,11 +17,23 @@ function setup() {
 
 }
 
-function paint(e) {
-    let pixel = e.target;
-    pixel.style.backgroundColor = 'white';
 
-    setTimeout(resetPixel,1000,pixel);
+function paint(e) {
+  let pixel = e.target;
+
+  let colorR = Math.random() * 255;
+  let colorG = Math.random() * 255;
+  let colorB = Math.random() * 255;
+
+  console.log(`Color R... ${colorR}`);
+  console.log(`Color G... ${colorG}`);
+  console.log(`Color B... ${colorB}`);
+
+  pixel.style.backgroundColor = `rgb(${colorR}, ${colorG}, ${colorB})`;
+
+  console.log(`Background color is: ${pixel.style.backgroundColor}`);
+
+  setTimeout(resetPixel, 2000, pixel);
 
 }
 
