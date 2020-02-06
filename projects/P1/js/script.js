3326d12f7b40a11ec730894eb32e37e4faaa0680
swishman <matthew23fishman@Gmail.com>
2020-02-06 01:11:27 -0500

    P1: Added +Configured positioning of Elements,

--- projects/P1/js/script.js
+++ projects/P1/js/script.js
@@ -2,49 +2,130 @@
 
 /********************************************************************
 
-Title of Project
-Author Name
+Explorations with scrollTop
+Matthew Fishman
 
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
+A parallax scrolling experiment.
 
 *********************************************************************/
 
+// Let's get this document ready.
+
 $(document).ready(setup);
+
+// Our setup function.
 
 function setup() {
 
-$(window).scroll(function() {
-  parallax();
 
-})
+  // Okay, so when the window scrolls, we're gonna make some things happen.
+
+  $(window).scroll(function() {
+
+    // Let's store how far away we move from the top of the window as we scroll down.
+
+    let scrollTop = $(window).scrollTop();
+
+    // Now, let's call our parallaxScroll function (see more details below).
+    // This function makes use of scrollTop to perform transformations to
+    // DOM elements.
+
+    parallaxScroll();
+
+    // We also want to keep track of how far we are along our story.
+    // Let's check and make sure with some if() statements.
+
+    // Our finale container.
+
+    if (scrollTop > $("#endcontainer").position().top) {
+      console.log(" red container");
+
+      // Here, we can perform functions when this container comes into view.
+
+      $("#endbutton")
+        .hide()
+        .fadeIn(1000);
+
+      $("#endbutton").click(function() {
+        window.location = '#start'
+      })
+
+    }
+
+    // Our midway through container.
+    else if (scrollTop > $("#middlecontainer").position().top) {
+      console.log(" yellow container");
+
+
+    }
+
+
+  })
+
+
+        $('#question').dialog({
+      buttons: {
+        "Good": function() {
+          $(this).dialog('close');
+        },
+        "Bad": function() {
+          $(this).parent().effect('shake');
+        }
+      }
+    });
+
 
 }
 
-function parallax() {
+function parallaxScroll() {
 
-  // Here, we'll calculate how far we've scrolled from the top of the window.
-  // When it loads, the window is at point 0.
+  // So, let's make sure scrolLTop is still what it needs to be here.
 
-  let scrollHeight = $(window).scrollTop();
+  let scrollTop = $(window).scrollTop();
   const HER_LOVE = 100;
   const HIS_LOVE = 25;
-  console.log("scrollHeight:" + scrollHeight);
+
+  // We can control the speed of the animation by changing the integer
+  // that we use to divide scrollTop.
 
   // Now, let's select the div containing our lovely wife, Merope.
   // Merope is contained inside a div with the ID, "girl".
-  // We'll use our scroll distance calculation to displace her position
+  // We'll use scrollTop to displace her position
   // through CSS.
 
-  // We can control the speed of the animation by changing the integer
-  // that we use to divide scrollHeight. We also multiply this by -1,
-  // so that Merope scrolls down, as we scroll down.
-
-  $("#girl").css("top", (1*scrollHeight/HER_LOVE));
+  $("#girl").css("top", scrollTop / HER_LOVE);
 
   // This box, contains Sisyphus. His div id is "boy".
   // Let's displace his position using the same function.
 
-  $("#boy").css("bottom", (1*scrollHeight/HIS_LOVE));
+  $("#boy").css("bottom", scrollTop / HIS_LOVE);
+
+  $("#text").css("transform", 'rotateY(' + (scrollTop / 100) + 'rad)');
+
+}
+
+
+  // This was some alternative math I experimented with.
+
+  // $("#girl").css("top", (1/1+Math.sqrt(scrollHeight)));
+  // console.log("top:: "+$("#girl").css("top"));
+  // let cTop = parseFloat($("#girl").css("top"))+(51/Math.sqrt(scrollHeight));
+  // $("#girl").css("top", cTop);
+  // console.log(cTop);
+  //
+  // console.log((1/Math.sqrt(scrollHeight)));
+
+
+
+// Here, we have a function which uses a CSS scale transformation
+// to apply some effects to elements.
+
+function parallaxGrow() {
+
+  $("girl").css("transform", 'scale(1,1)');
+  let cTop = parseFloat($("#girl").css("transform")) + ('scale(' + (-1 * scrollHeight) + ')');
+
+  $("#girl").css("transform", cTop);
+
 
 }
