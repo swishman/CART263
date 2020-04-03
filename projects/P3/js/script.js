f12229cf8de687b239e99186edb994da5cb29f4a
swishman <matthew23fishman@Gmail.com>
2020-04-02 23:31:51 -0400

    P3: Started
    Configured Ascii, Added UI Tabs

---
+++ projects/P3/js/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/********************************************************************
+
+Title of Project
+Author Name
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+
+$(document).ready(setup);
+
+
+function setup() {
+
+  $.ajax({
+    url: '../assets/data/gundam.txt', // Location of the file
+    dataType: 'text' // The type of data we're requesting
+  })
+  .done(gotData)
+  .fail(dataError);
+
+  $( function() {
+   $( "#tabs" ).tabs();
+ } );
+
+}
+
+function gotData(data) {
+  startTyping(data);
+}
+
+function dataError() {
+  console.error("Boo!");
+}
+
+function startTyping(text) {
+  let next = 10;
+  let typingInterval = setInterval(function() {
+    $('#gundam').append(text[next]);
+    next++;
+    if (next === text.length) {
+      clearInterval(typingInterval);
+    }
+  }, 0.1);
+
+}
